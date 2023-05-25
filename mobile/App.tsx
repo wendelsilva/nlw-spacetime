9843707ea8b552d2dd4b1ebdaf09d0c6f1fdef3a
wendelsilva <wendel245.ws@gmail.com>
2023-05-25 15:02:54 -0300

    config tailwind mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-gray-50 font-bold text-5xl">Hello world</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text font-alt text-5xl">Hello world</Text>
       <StatusBar style="light" translucent />
     </View>
   )
